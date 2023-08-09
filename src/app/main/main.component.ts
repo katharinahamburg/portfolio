feaddcd5a9f627996f49b3d8d7aeb245f68412f2
katharinahamburg <katharina@hinnrichs.de>
2023-08-09 15:52:56 +0200

    animations added

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
- public arrow = [];
+export class MainComponent implements OnInit {
+arrows = ['arrowdown1.svg', 'arrowdown2.svg', 'arrowdown3.svg', 'arrowdown4.svg', 'arrowdown5.svg'];
+currentArrow = 0;
+showArrow = true;
 
- constructor() {
-  for (let i = 0; i < 4; i++) {
- this.arrow.push('/assets/img/arrowdown_' + i);
+ngOnInit() {
+  this.updateArrow();
+}
+
+updateArrow()  {
+  setInterval(() => {
+    this.currentArrow++;
+    this.currentArrow = this.currentArrow % this.arrows.length;
+    this.showArrow = false;
+
+    setTimeout(() => {
+      this.showArrow = true;
+    }, 10);
+  }, 400);
   }
- }
 
 }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit {
 
+rightarrows = ['arrowright1.svg', 'arrowright2.svg', 'arrowright3.svg'];
+leftarrows = ['arrowleft1.svg', 'arrowleft2.svg', 'arrowleft3.svg'];
+currentSkillArrow = 0;
+showSkillArrow = true;
+
+ngOnInit() {
+  this.updateSkillArrow();
+}
+
+updateSkillArrow()  {
+
+  setInterval(() => {
+    this.currentSkillArrow++;
+    this.currentSkillArrow = this.currentSkillArrow % this.rightarrows.length;
+    this.currentSkillArrow = this.currentSkillArrow % this.leftarrows.length;
+    this.showSkillArrow = false;
+
+    setTimeout(() => {
+      this.showSkillArrow = true;
+    }, 10);
+  }, 400);
+  }
 }
