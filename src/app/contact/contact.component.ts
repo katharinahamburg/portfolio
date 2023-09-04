a305b287e289169d3150653bc537d0b239b94517
katharinahamburg <katharina@hinnrichs.de>
2023-09-04 14:39:22 +0200

    form reviewed

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -10,31 +12,31 @@ export class ContactComponent {
   @ViewChild('nameField') nameField!: ElementRef
   @ViewChild('mailField') mailField!: ElementRef
   @ViewChild('messageField') messageField!: ElementRef
-  @ViewChild('button') button!: ElementRef
+  @ViewChild('sendButton') sendButton!: ElementRef
 
 
   async sendMail() {
     let nameField = this.nameField.nativeElement;
     let mailField = this.mailField.nativeElement;
     let messageField = this.messageField.nativeElement;
-    let button = this.button.nativeElement;
+    let sendButton = this.sendButton.nativeElement;
 
     nameField.disabled = true;
     mailField.disabled = true;
     messageField.disabled = true;
-    button.disabled = true;
+    sendButton.disabled = true;
 
     let fd = new FormData();
     fd.append('name', nameField.value);
     fd.append('message', messageField.value);
     fd.append('email', mailField.value);
 
-    await fetch('https://katharina@hinnrichs.de/send_mail/send_mail.php',
+    await fetch('https://hinnrichs.de/send_mail.php',
       {
         method: 'POST',
         body: fd
       }
-    )
+    );
     
   }
 
